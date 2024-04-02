4a8f0e67706d4b45c03510e7fe5079f545756e1f
ErikaGM <erikat.gamboam@konradlorenz.edu.co>
2024-04-01 20:13:34 -0500

    Registro carros 'venta'

--- users/admin.py
+++ users/admin.py
@@ -4,15 +4,6 @@ from users.models import UsersModel
 # Register your models here.
 class UsersAdmin(admin.ModelAdmin):
     list_display = ["nombre","apellido","paisUser","ciudad","gen","fechNac", "barrio"]
-    def formfield_for_choice_field(self, db_field, request, **kwargs):
-        if db_field.name == "barrio":
-            # Obtén el objeto del usuario actual
-            obj = kwargs.get("obj")
-            if obj and obj.ciudad:
-                # Filtra los barrios según la ciudad seleccionada
-                kwargs["choices"] = [(barrio, barrio) for barrio in obj.obtener_barrios()]
-        return super().formfield_for_choice_field(db_field, request, **kwargs)
-   
-   
+    
   
 admin.site.register(UsersModel, UsersAdmin)
--- users/models.py
+++ users/models.py
@@ -1,47 +1,25 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 class UsersModel(models.Model):
-    print ("entra model user")
-    nombre= models.CharField(max_length=50)
-    apellido= models.CharField(max_length=50)
-
-    paisUser= models.CharField(max_length=40,  default= 'Colombia')
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    paisUser = models.CharField(max_length=40, default='Colombia')
+    
     GENERO = (
-         ('h', 'Hombre'),
+        ('h', 'Hombre'),
         ('m', 'Mujer')  
-        )
-    gen = models.CharField(max_length=5, choices=GENERO, default= '' )
-    fechNac = models.DateField(blank=True, null=True)
-    CIUDAD = (
-        ('bogota', 'Bogota'),
-        ('medellin', 'Medellin'), 
-        ('cali', 'Cali') 
     )
-    
-    ciudad = models.CharField(max_length=30, choices=CIUDAD, default= '' )
-    
-    BARRIOS = {
-        'bogota': [('barrio1_bogota', 'Barrio1_Bogota'), ('barrio2_bogota', 'Barrio2_Bogota'), ('barrio3_bogota', 'Barrio3_Bogota')],
-        'medellin': [('barrio1_medellin', 'Barrio1_Medellin'), ('barrio2_medellin', 'Barrio2_Medellin'), ('barrio3_medellin', 'Barrio3_Medellin')],
-        'cali': [('barrio1_cali', 'Barrio1_Cali'), ('barrio2_cali', 'Barrio2_Cali'), ('barrio3_cali', 'Barrio3_Cali')],
-    }
-    
-    barrio = models.CharField(max_length=100, choices=[], default='')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.actualizar_opciones_barrio()
-
-    def actualizar_opciones_barrio(self):
-        self._meta.get_field('barrio').choices = self.BARRIOS.get(self.ciudad, [])
-
-
-
-
-
-
-
-
-
-
-
+    gen = models.CharField(max_length=5, choices=GENERO, default='')
+    fechNac = models.DateField(blank=True, null=True)
+    ciudad = models.CharField(max_length=40, default='Bogota')
+ 
+    BARRIOS = (
+        ('barrioB1', 'Chapinero'),
+        ('barrioB2', 'Usaquen'), 
+        ('barrioB3', 'Suba') ,
+        ('barrioB4', 'Kennedy') ,
+        ('barrioB5', 'Usme') ,
+    )
+    barrio = models.CharField(max_length=100, choices=BARRIOS)
